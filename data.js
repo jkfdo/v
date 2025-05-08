73a25d713fea7e890ffb5d1aa3d6e15acd8cb28a
jkfdo <obidev@proton.me>
2025-05-09 01:47:13 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,181 @@
+const data = [
+  {
+    name: `Gravity`,
+    artist: `The Ambientalist`,
+    subArtist: ``,
+    genre: `Chillstep Chillout Ambient Electronic`,
+    mood: `Emotional Sorrow Sad Alone Dreamy Afternoon NewWorld Solitude Urban`,
+    
+    channel: `AurVen`,
+    publishDate: `6 May 2025`,
+    
+    pbLnk: `https://youtu.be/DvBba16NFAA`,
+    arLnk: `https://youtu.be/p06TdtcBtbc`,
+    bolume: `AurNov`,
+    
+    
+    artwork: `https://www.deviantart.com/brokenteapotstudios/art/Shooting-Stars-1166390070`,
+    art: `https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/87adf66c-7944-4128-8b9b-d892d63090b4/djafsc6-e5922f83-8f87-4375-b9e5-a4ac22e1df2b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg3YWRmNjZjLTc5NDQtNDEyOC04YjliLWQ4OTJkNjMwOTBiNFwvZGphZnNjNi1lNTkyMmY4My04Zjg3LTQzNzUtYjllNS1hNGFjMjJlMWRmMmIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.AwTRt88YfbZ-iDdiTSq5GHS-BO5LFKkekxj2ymEI0fQ`,
+  },
+  {
+    name: `The Tuning Of The Tide `,
+    artist: `Oscuro`,
+    subArtist: ``,
+    genre: `Chillstep Chillout Ambient `,
+    mood: `Alone Emotional Sad Lonely Afternoon Sunset Evening Dreamy Solitude Sorrow NewWorld Lake `,
+    
+    channel: `AurVen`,
+    publishDate: `8 May 2025`,
+    
+    pbLnk: `https://youtu.be/0d1KaU9hw6E`,
+    arLnk: `https://youtu.be/Kgj3B0jSOlU`,
+    bolume: `AurNov`,
+    
+    
+    artwork: `https://www.deviantart.com/eoworfindir/art/Beach-1174903793`,
+    art: `https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0fb89d67-8857-4fc5-b3a2-4345ac0c7111/djfi9kh-72aa958a-7d97-4209-a4cb-9d8359bb39f2.png/v1/fill/w_1280,h_718,q_80,strp/beach_by_eoworfindir_djfi9kh-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzE4IiwicGF0aCI6IlwvZlwvMGZiODlkNjctODg1Ny00ZmM1LWIzYTItNDM0NWFjMGM3MTExXC9kamZpOWtoLTcyYWE5NThhLTdkOTctNDIwOS1hNGNiLTlkODM1OWJiMzlmMi5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.0PJLIvX6R5csvPBr9AnNDj-G3XXMbmRfViTOdri6V0I`,
+  },
+  {
+    name: ``,
+    artist: ``,
+    subArtist: ``,
+    genre: ``,
+    mood: ``,
+    
+    channel: `AurVen`,
+    publishDate: `10 May 2025`,
+    
+    pbLnk: ``,
+    arLnk: ``,
+    bolume: `AurNov`,
+    
+    
+    artwork: ``,
+    art: ``,
+  },
+  {
+    name: ``,
+    artist: ``,
+    subArtist: ``,
+    genre: ``,
+    mood: ``,
+    
+    channel: `AurVen`,
+    publishDate: `12 May 2025`,
+    
+    pbLnk: ``,
+    arLnk: ``,
+    bolume: `AurNov`,
+    
+    
+    artwork: ``,
+    art: ``,
+  },
+  {
+    name: ``,
+    artist: ``,
+    subArtist: ``,
+    genre: ``,
+    mood: ``,
+    
+    channel: `AurVen`,
+    publishDate: `14 May 2025`,
+    
+    pbLnk: ``,
+    arLnk: ``,
+    bolume: `AurNov`,
+    
+    
+    artwork: ``,
+    art: ``,
+  },
+  
+  /*
+  {
+    name: ``,
+    artist: ``,
+    subArtist: ``,
+    genre: ``,
+    mood: ``,
+    
+    channel: `AurVen`,
+    publishDate: ``,
+    pubLink = ``,
+    
+    srcLink = ``,
+    srcVolume = ``,
+    
+    artwork: ``,
+    art: ``,
+  },
+  
+   {
+     name: ``,
+     artist: ``,
+     subArtist: ``,
+     genre: ``,
+     mood: ``,
+     
+     channel: `AurVen`,
+     publishDate: ``,
+     pubLink = ``,
+     
+     srcLink = ``,
+     srcVolume = ``,
+     
+     artwork: ``,
+     art: ``,
+   },
+   {
+     name: ``,
+     artist: ``,
+     subArtist: ``,
+     genre: ``,
+     mood: ``,
+     
+     channel: `AurVen`,
+     publishDate: ``,
+     pubLink = ``,
+     
+     srcLink = ``,
+     srcVolume = ``,
+     
+     artwork: ``,
+     art: ``,
+   },
+   {
+     name: ``,
+     artist: ``,
+     subArtist: ``,
+     genre: ``,
+     mood: ``,
+     
+     channel: `AurVen`,
+     publishDate: ``,
+     pubLink = ``,
+     
+     srcLink = ``,
+     srcVolume = ``,
+     
+     artwork: ``,
+     art: ``,
+   },
+   {
+     name: ``,
+     artist: ``,
+     subArtist: ``,
+     genre: ``,
+     mood: ``,
+     
+     channel: `AurVen`,
+     publishDate: ``,
+     pubLink = ``,
+     
+     srcLink = ``,
+     srcVolume = ``,
+     
+     artwork: ``,
+     art: ``,
+   },
+   */
+]
